b9d22e1d21eb1655a94eaedf5ee749556fda809d
soulirove <80773072+soulirove@users.noreply.github.com>
2021-10-27 17:56:57 +0200

    Blob function added
    now the cars are blobbed for ease of use in the future

--- OpenCVcourse/Test.cpp
+++ OpenCVcourse/Test.cpp
@@ -1,3 +1,172 @@
+<<<<<<< HEAD
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video.hpp>
+#include <iostream>
+#include <sstream>
+
+
+using namespace cv;
+using namespace std;
+
+// global variables ///////////////////////////////////////////////////////////////////////////////
+const Scalar BLACK = Scalar(0, 0, 0);
+const Scalar WHITE = Scalar(255, 255, 255);
+const Scalar YELLOW = Scalar(0, 255, 255);
+const Scalar GREEN = Scalar(0, 200, 0);
+const Scalar RED = Scalar(0, 0, 255);
+double dsit2Threshold, varThreshold;
+int history;
+bool detectShadows;
+Mat img, mask, imgCrop, maskThreshold;
+Ptr<BackgroundSubtractor> object_detector;
+
+class Blob {
+public:
+    // member variables ///////////////////////////////////////////////////////////////////////////
+    vector<Point> contour;
+
+    Rect boundingRect;
+
+    Point centerPosition;
+
+    double dblDiagonalSize;
+
+    double dblAspectRatio;
+
+    // function prototypes ////////////////////////////////////////////////////////////////////////
+    Blob(vector<Point> _contour);
+
+};
+
+Blob::Blob(vector<Point> _contour) {
+
+    contour = _contour;
+
+    boundingRect = cv::boundingRect(contour);
+
+    centerPosition.x = (boundingRect.x + boundingRect.x + boundingRect.width) / 2;
+    centerPosition.y = (boundingRect.y + boundingRect.y + boundingRect.height) / 2;
+
+    dblDiagonalSize = sqrt(pow(boundingRect.width, 2) + pow(boundingRect.height, 2));
+
+    dblAspectRatio = (float)boundingRect.width / (float)boundingRect.height;
+
+}
+
+
+////////////////////////////////// functions ////////////////////////////////////////////////////////////////////////////
+void matchCurrentFrameBlobsToExistingBlobs(std::vector<Blob>& existingBlobs, std::vector<Blob>& currentFrameBlobs);
+void addBlobToExistingBlobs(Blob& currentFrameBlob, std::vector<Blob>& existingBlobs, int& intIndex);
+void addNewBlob(Blob& currentFrameBlob, std::vector<Blob>& existingBlobs);
+double distanceBetweenPoints(cv::Point point1, cv::Point point2);
+void drawAndShowContours(cv::Size imageSize, std::vector<std::vector<cv::Point> > contours, std::string strImageName);
+void drawAndShowContours(cv::Size imageSize, std::vector<Blob> blobs, std::string strImageName);
+void drawBlobInfoOnImage(std::vector<Blob>& blobs, cv::Mat& imgFrame2Copy);
+
+
+
+/////////////////////// Images ///////////////////////
+
+int main(){
+
+    string path = "Resources/highway.mp4";
+    VideoCapture cap(path);
+
+
+    /*object detection from stable camera */
+    //object_detector = createBackgroundSubtractorKNN(history = (500) , dsit2Threshold  =(400.0), detectShadows = true);
+    object_detector = createBackgroundSubtractorMOG2(history = (500) , varThreshold=(30.0), detectShadows = true);
+
+    while (true) {
+        cap.read(img);
+
+        /*extract region of interest*/
+        int width = img.cols;
+        int height = img.rows;
+        cout << "Width : " << width << "Height: " << height << endl;
+        Rect roi(150, 300, 850, 420);
+        imgCrop = img(roi);
+
+
+        /*object detection */
+        //object_detector->apply(img, mask);  //no need for this part any more 
+        object_detector->apply(imgCrop, mask);// aplying the object detection only in the roi 
+        threshold(mask, maskThreshold, 254, 255, THRESH_BINARY);
+
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+        //findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE); //mask with shadows 
+        findContours(maskThreshold, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);//mask without the shadows 
+        Mat structuringElement3x3 = getStructuringElement(MORPH_RECT, cv::Size(3, 3)); // setting up a 3x3 kernel 
+        Mat opening, closing;
+        morphologyEx(maskThreshold, closing, 3, structuringElement3x3,Point(-1, -1),5); // preforming a morphological change closing 5 times 
+        morphologyEx(closing, opening, 2, structuringElement3x3, Point(-1, -1), 1); // preforming a morphological change openning once
+        findContours(opening, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE); // finding the contours in the opening matrix 
+        vector<vector<Point> >hull(contours.size());
+        for (size_t i = 0; i < contours.size(); i++)
+        {
+            convexHull(contours[i], hull[i]); // setting the boundries of our blobs 
+        }
+        vector<Blob> blobs;
+        for (auto& hull : hull) {
+            Blob possibleBlob(hull);
+
+            if (possibleBlob.boundingRect.area() > 1000 &&
+                possibleBlob.dblAspectRatio >= 0.5 &&
+                possibleBlob.dblAspectRatio <= 2 &&
+                possibleBlob.boundingRect.width > 30 &&
+                possibleBlob.boundingRect.height > 40 &&
+                possibleBlob.dblDiagonalSize > 80.0  ) {
+                blobs.push_back(possibleBlob); // selecting the blobs we want and saving them for later 
+            }
+        }
+        Mat imgHulls(opening.size(), CV_8UC3, BLACK);
+
+        hull.clear();
+
+        for (auto& blob : blobs) {
+            hull.push_back(blob.contour);
+        }
+        drawContours(imgHulls, hull, -1, WHITE, -1);
+
+        imshow("imgHulls", imgHulls);
+
+
+        for (auto& blob : blobs) {                                                  // for each blob
+            rectangle(imgCrop, blob.boundingRect, RED, 2);             // draw a red box around the blob
+            int tlx = blob.boundingRect.tl().x;
+            int tly = blob.boundingRect.tl().y;
+            int brx = blob.boundingRect.br().x;
+            int bry = blob.boundingRect.br().y;
+            cout << "Top left point : " << blob.boundingRect.tl() << " Buttom right point : " << blob.boundingRect.br() << endl;
+            cout << "Top left point X : " << tlx << endl;
+            cout << "Top left point Y : " << tly << endl;
+            cout << "Buttom right point X : " << brx << endl;
+            cout << "Buttom right point Y : " << bry << endl;
+            circle(imgCrop, blob.centerPosition, 3, GREEN, -1);  // draw a filled-in green circle at the center
+            int cenX = blob.centerPosition.x;
+            int cenY = blob.centerPosition.y;
+            cout << "Center point X: " << blob.centerPosition.x << " Center point Y: " << blob.centerPosition.y << endl;
+        }   
+
+        //show the current frame and the mask
+        imshow("video",img);
+        imshow("mask", maskThreshold);
+        imshow("crop", imgCrop);
+        //imshow("Hull demo", drawing);
+
+        //get the input from the keyboard
+        int keyboard = waitKey(24);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+    return 0;
+=======
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -91,4 +260,5 @@ int main(){
             break;
     }
     return 0;
+>>>>>>> f016217e0421be841d7d0fbb8835bab833c453f5
 }
